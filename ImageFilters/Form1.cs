74a779f2f1341b6c49396ba3d105c83045b54489
Joussiana <jousaina.atef@gmail.com>
2026-05-12 16:32:52 +0300

    Implement Median and Midpoint filters with performance graphs using ZedGraph and waiting for the counting filter code there was a small mistake in the  buttons' name in Form design (eff and non-eff) i swapped the names so please check them again

--- ImageFilters/Form1.cs
+++ ImageFilters/Form1.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
@@ -45,6 +46,7 @@ namespace ImageFilters
         private void btnOpen_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 //Open the browsed image and display it
@@ -211,5 +213,70 @@ namespace ImageFilters
 
             MessageBox.Show("Median Filter (Quick Sort) Applied!\nWindow Size: " + windowSize + "x" + windowSize + "\nExecution Time: " + executionTime + " ms");
         }
+
+
+
+
+
+
+
+        private void analyze_Click(object sender, EventArgs e)
+        {
+            if (ImageMatrix == null)
+            {
+                MessageBox.Show("Please open an image first!");
+                return;
+            }
+            double[] window_sizes = { 3, 5, 7 };
+            //to store time to put on y-axis
+            double[] quick_y = new double[3];
+            double[] counting_y = new double[3];
+            double[] kth_y = new double[3];
+            double[] MidpointEfficient_y= new double[3];
+            double[] NonEfficientMidPoint_y = new double[3];
+
+            // calculate time taken for each filter at each window 
+            Stopwatch sw = new Stopwatch();
+            for(int i = 0; i < window_sizes.Length; i++)
+            {
+                int currentSize = (int)window_sizes[i];
+                //time of quick
+                sw.Restart();
+                ImageFilters.ImageOperations.MedianQuickSortFilter(ImageMatrix,currentSize);
+                sw.Stop();
+                quick_y[i] = sw.ElapsedMilliseconds;
+
+                //time of counting
+                // sw.Restart();
+                //ImageFilters.ImageOperations.MedianCountingFilter(ImageMatrix, currentSize);
+                //sw.Stop();
+                //counting_y[i] = sw.ElapsedMilliseconds;
+
+                //time of kth
+                sw.Restart();
+                ImageFilters.ImageOperations.KthFilter(ImageMatrix,currentSize);
+                sw.Stop();
+                kth_y[i] = sw.ElapsedMilliseconds;
+
+                //time of Midpoint Efficient
+                sw.Restart();
+                ImageFilters.ImageOperations.ApplyMidpointEfficient(ImageMatrix, currentSize);
+                sw.Stop();
+                MidpointEfficient_y[i] = sw.ElapsedMilliseconds;
+
+                //time of Midpoint Efficient
+                sw.Restart();
+                ImageFilters.ImageOperations.ApplyMidpointNonEfficient(ImageMatrix, currentSize);
+                sw.Stop();
+                NonEfficientMidPoint_y[i] = sw.ElapsedMilliseconds;
+            }
+
+            Form2 graphForm = new Form2();
+            graphForm.DrawAllGraphs(window_sizes, quick_y, kth_y, MidpointEfficient_y, NonEfficientMidPoint_y);
+            graphForm.Show();
+        }
     }
 }
+
+
+
